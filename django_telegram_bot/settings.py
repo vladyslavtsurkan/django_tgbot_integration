36de3045249dedb480f29e62f4ad554d79b4f904
vladyslavtsurkan <vtsurkan2002@gmail.com>
2024-01-03 21:11:43 +0200

    Preparing for deploy

--- django_telegram_bot/settings.py
+++ django_telegram_bot/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
+
+import dj_database_url
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +28,11 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+if not DEBUG:
+    load_dotenv("/etc/secrets/.env")
+    CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS").split(",")
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -84,10 +91,7 @@ WSGI_APPLICATION = 'django_telegram_bot.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.getenv("DJANGO_DATABASE_URL"))
 }
 
 
